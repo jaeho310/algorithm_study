fcbcf3ed2aa4cd7c3dede401e6ccd2c35fd8f735
jaeho choi <chlwogh1993@gmail.com>
2021-12-27 18:15:45 +0900

    commit

--- b_1052.py
+++ b_1052.py
@@ -1,18 +1,17 @@
-from sys import stdin
+N,K=map(int,input().split())
 
-def solution(n, k, cnt, water):
-    if n % 2 != 0:
-        cnt += water
-        n += 1
-    # next_bottle = n//2
-    # if next_bottle % 2 != 0:
-        # cnt += rec_cnt * 2
-        # next_bottle += 1
-    if n <= k:
-        return cnt
-    cnt = solution(next_bottle, k, cnt, rec_cnt+1)
-    return cnt
+def cheak(bottle_cnt):
+    ans = 0
+    while True:
+        bottle_cnt, remain = divmod(bottle_cnt, 2)
+        ans += remain
+        num = bottle_cnt
+        if num == 0:
+            return ans
 
-if __name__ == '__main__':
-    n, k = map(int, stdin.readline().split())
-    print(solution(n, k, 0, 1))
+bottle_cnt = N
+while True:
+    if cheak(bottle_cnt) <= K:
+        print(bottle_cnt-N)
+        break
+    bottle_cnt += 1
