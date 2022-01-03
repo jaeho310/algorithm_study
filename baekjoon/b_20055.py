d06cf125afc80cb983541f7709bc8ca966c82a45
jaeho choi <chlwogh1993@gmail.com>
2022-01-03 18:18:51 +0900

    commit

--- baekjoon/b_20055.py
+++ baekjoon/b_20055.py
@@ -1,10 +1,30 @@
-from collections import deque
+N,K = map(int,input().split())
 
-if __name__ == '__main__':
-    n, k = map(int, input().split())
-    queue = deque(map(int, input().split()))
-    cnt = 0
-    while True:
-        data = queue.popleft()
-        queue.append(data)
-        cnt += 1
+naegoo = list(map(int,input().split()))
+
+robot=[0]*N
+
+step=0
+while(naegoo.count(0)<K):
+    step+=1
+    a=naegoo.pop()
+    naegoo.insert(0,a)
+
+    robot.pop()
+    robot.insert(0,0)
+
+    robot[N - 1] = 0
+
+    for i in range(N-2,0,-1):
+        if robot[i] and naegoo[i+1] and (not robot[i+1]):
+            robot[i]=0
+            robot[i+1]=1
+            naegoo[i+1] = max(0,naegoo[i+1]-1)
+
+    robot[N - 1] = 0
+
+    if naegoo[0] and (not robot[0]):
+        robot[0]=1
+        naegoo[0] = max(0,naegoo[0]-1)
+
+print(step)
--- baekjoon/p_1904.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-byte_value = bin(n)
-answer = 0
-for i in byte_value:
-    if i == '1':
-        answer += 1
-print(answer)
