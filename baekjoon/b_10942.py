cb88e47c30d9a1b89355dcbbdd306771bf383d21
jaeho choi <chlwogh1993@gmail.com>
2022-01-04 17:22:52 +0900

    commit

--- baekjoon/b_10942.py
+++ baekjoon/b_10942.py
@@ -1,42 +1,31 @@
-from collections import defaultdict
+import sys
 
-dp = defaultdict(list)
+input = sys.stdin.readline
 
-# def check(order, dp):
-#     if arr[order[0]] != arr[order[1]]:
-#         dp[order] = False
-#     else:
-#         dp[order] = True
+n = int(input())
+numbers = list(map(int, input().split()))
+m = int(input())
 
-def solution(arr, dp, start, end):
-    break_time = (end - start) // 2
-    cnt = 0
-    while cnt < break_time:
-        if end-cnt-1 not in dp[arr[start+cnt-1]]:
-            return 0
-        cnt += 1
-    return 1
+# dp
+dp = [[0] * n for _ in range(n)]
 
-if __name__ == '__main__':
-    n = int(input())
-    arr = list(map(int, input().split()))
-    for el in arr:
-        if el not in dp:
-            res = [i for i, value in enumerate(arr) if value == el]
-            dp[el] = res
-    m = int(input())
-    for _ in range(m):
-        start, end = map(int, input().split())
-        print(solution(arr, dp, start, end))
-# 7
-# 1 2 1 3 1 2 1
-# 4
-# 1 3
-# 2 5
-# 3 3
-# 5 7
+for num_len in range(n):
+    for start in range(n - num_len):
+        end = start + num_len
 
-# 1
-# 0
-# 1
-# 1
+        # 시작점과 끝점이 같다면 글자수가 1개이므로 무조건 팰린드롬
+        if start == end:
+            dp[start][end] = 1
+        # 시작점의 글자와 끝점의 글자가 같다면
+        elif numbers[start] == numbers[end]:
+            # 두 글자짜리 문자열이라면 무조건 팰린드롬
+            if start + 1 == end:
+                dp[start][end] = 1
+            # 가운데 문자열이 팰린드롬이라면 팰린드롬
+            elif dp[start + 1][end - 1] == 1:
+                dp[start][end] = 1
+
+# 정답출력하기
+for question in range(m):
+    s, e = map(int, input().split())
+    print(dp[s - 1][e - 1])
