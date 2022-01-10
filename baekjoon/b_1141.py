3b1ddace5cbe10f2d410b7c8647bd9615df85be4
jaeho choi <chlwogh1993@gmail.com>
2022-01-10 17:06:14 +0900

    commit

--- baekjoon/b_1141.py
+++ baekjoon/b_1141.py
@@ -1,23 +1,22 @@
-if __name__ == '__main__':
-    n = int(input())
-    queue = list()
-    for _ in range(n):
-        queue.append(input())
-    queue.sort(key=lambda x: len(x))
-    answer = 0
-    last_data = queue[-1]
-    while queue:
-        cnt = 0
-        data = queue.pop(0)
-        for i in queue:
-            is_contain = True
-            for j in range(len(data)):
-                if data[j] != i[j]:
-                    is_contain = False
-                    break
-            if not is_contain:
-                cnt += 1
-        answer = max(answer, cnt+1)
-        if data == last_data:
-            break
-    print(answer)
+N=int(input())
+L=[]
+for i in range(N):
+    word=input()
+    L.append(word)
+
+L.sort(key=len)
+ans=0
+for i in range(N):
+    nowWord=L[i]
+    isHead=False
+    for j in range(i+1,N):
+        try:
+            if L[j].index(nowWord)==0:
+                isHead=True
+                break
+        except:
+            continue
+    if not isHead:
+        #print(nowWord)
+        ans+=1
+print(ans)
